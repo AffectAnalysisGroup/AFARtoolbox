a1c73d04cf217db7de393110b99129106b18e483
Wanqiao Ding <wanqiaod@gmail.com>
2019-01-24 21:54:54 -0500

    Update runZface.m

--- mfile/runZface.m
+++ mfile/runZface.m
@@ -8,17 +8,18 @@ function runZface(video_dir,zface)
 
     for file_index = 1 : file_num
 
-    	if video_dir_list(file_index).isdir
-    		continue
-    	end
-
     	video_name    = video_dir_list(file_index).name;
     	[~,fname,ext] = fileparts(video_name);
     	video_path 	  = fullfile(video_dir,video_name);
-    	fit_path      = fullfile(zface.matOut,[fname '_fit.mat']);
+        
+        if ~isVideoFile(video_path)
+            continue
+        end
+        
+    	fit_path          = fullfile(zface.matOut,[fname '_fit.mat']);
 	    zface_video_path  = fullfile(zface.videoOut,[fname '_zface' ext]);
     	runZfaceSingleVideo(zface,video_path,zface_video_path,fit_path);
-    	
+        
     end
 
 end
