b926a3b98a1c21456efdffd8c9875a42a7cafa76
Wanqiao Ding <wanqiaod@gmail.com>
2019-03-01 12:10:58 -0500

    Add test scripts.

---
+++ mfile/test_run_pipeline.m
@@ -0,0 +1,24 @@
+% Testing runZface.m
+
+video_dir    = '/Users/wanqiaod/workspace/pipeline/test_video';
+output_dir   = '/Users/wanqiaod/workspace/pipeline/out';
+zface_folder = '/Users/wanqiaod/workspace/pipeline/zface';
+FETA_folder  = '/Users/wanqiaod/workspace/pipeline/FETA';
+AU_folder    = '/Users/wanqiaod/workspace/pipeline/AU_detector';
+run_zface    = true;
+run_FETA     = true;
+run_AU       = false;
+create_out   = true;
+% % Test initOutDir()
+% [zface,FETA,AU] = initOutDir(zface_folder,FETA_folder,AU_folder,output_dir,...
+% 							 false);
+% addpath(genpath('.'));
+
+% runZface(video_dir,zface);
+
+
+
+runPipeline(video_dir,output_dir,zface_folder,FETA_folder,AU_folder,...
+            run_zface,run_FETA,run_AU,create_out);
+
+
