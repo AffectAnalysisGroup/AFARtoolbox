cd2166f052dc74de3b6771f133fe85bcbcff630e
Wanqiao Ding <wanqiaod@gmail.com>
2019-04-06 21:25:46 -0400

    Deleted unused files

--- visualization/testPlotAU.m
+++
@@ -1,6 +0,0 @@
-auIndexVect = [1,2,3,4,5,7];
-origVideoPath = '/Users/wanqiaod/workspace/pipeline/test_video/LeBlanc_short.mp4';
-auDir = '/Users/wanqiaod/workspace/pipeline/out/AU_detector_out/';
-normAnnotatedVideoDir = '/Users/wanqiaod/workspace/pipeline/out/feta_out/feta_norm_annotated_videos/';
-
-plotAU(auIndexVect,origVideoPath,auDir,normAnnotatedVideoDir);
