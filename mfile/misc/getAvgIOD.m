7ec0e7fb2c8fe7735619724013c8496f55809bdd
Wanqiao Ding <wanqiaod@gmail.com>
2019-04-08 18:57:10 -0400

    Add 2 helper functions.

---
+++ mfile/misc/getAvgIOD.m
@@ -0,0 +1,12 @@
+function avgIOD = getAvgIOD(fit)
+
+  total_frame = size(fit,2);
+  IOD = zeros(1,total_frame);
+  for frame_index = 1 : size(fit,2)  
+      left_corner_dist = 
+      current_IOD = 
+      IOD(frame) 
+      continue;
+  end
+
+end
---
+++ mfile/misc/getPntDist.m
@@ -0,0 +1,3 @@
+function dist = getPntDist(p1, p2)
+  dist = sqrt((p2(1)-p1(1))^2 + (p2(2)-p1(2))^2);
+end
