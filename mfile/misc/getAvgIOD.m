ec801e4fe05b512950ebfa5780cec7504b426191
Wanqiao Ding <wanqiaod@gmail.com>
2019-04-08 19:17:50 -0400

    Updates on avg dist/IOD functions.

--- mfile/misc/getAvgIOD.m
+++ mfile/misc/getAvgIOD.m
@@ -2,11 +2,13 @@ function avgIOD = getAvgIOD(fit)
 
   total_frame = size(fit,2);
   IOD = zeros(1,total_frame);
-  for frame_index = 1 : size(fit,2)  
-      left_corner_dist = 
-      current_IOD = 
-      IOD(frame) 
-      continue;
+  for frame_index = 1 : total_frame 
+      single_face = fit(frame_index).pts_2d(20:31,:);
+      left_dist   = pdist([single_face(1,:);single_face(7,:)],'euclidean');
+      right_dist  = pdist([single_face(4,:);single_face(10,:)],'euclidean');
+      current_IOD = mean([left_dist right_dist]);
+      IOD(frame)  = current_IOD;
   end
+  avgIOD = mean(IOD);
 
 end
