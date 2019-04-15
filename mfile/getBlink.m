b40d6df6746fcfad3365becb73edd492cb06f298
Wanqiao Ding <wanqiaod@gmail.com>
2019-04-15 11:08:49 -0400

    Update blink interval.

--- mfile/getBlink.m
+++ mfile/getBlink.m
@@ -1,4 +1,4 @@
-function blink = getBlink(fit,blink_threhold)
+function blink = getBlink(fit,blink_threshold)
 % getBlink takes fit(output from zface) and a threshold of eyelid distance.
 %   fit: struct, output from zface. Or maybe output from FETA for normalization
 %   blink_threshold: double, a threshold for eyelid distance. If the distance 
@@ -13,10 +13,10 @@ frame_rate      = 30;
 normalizeDist   = true;
 
 % the number of frames in the original video
-frame_cnt = size(fit,2);
+total_frame = size(fit,2);
 % get average distance between eye lids in every frame
-avg_dist = zeros(frame_cnt,1);
-for frame = 1 : frame_cnt
+avg_dist = zeros(total_frame,1);
+for frame = 1 : total_frame
     % single_face is the 2D coordinates of all the landmarkers on face.
     single_face = fit(frame).pts_2d;
     if isempty(single_face)
@@ -35,13 +35,15 @@ if normalizeDist == false
 end
 avg_dist = avg_dist/avg_IOD;
 
-for frame = 2 : (frame_cnt-1)
+for frame = 2 : (total_frame-1)
     prev_dist = avg_dist(frame-1);
     next_dist = avg_dist(frame+1);
     curr_dist = avg_dist(frame);
     % if the current distance is lower than the threshold and a local minimum
     % then we consider it as a blink.
-    if curr_dist < blink_threhold &  curr_dist < next_dist & curr_dist < prev_dist
+    is_local_min = (curr_dist < next_dist & curr_dist < prev_dist);
+    nonzero_below_threshold = (curr_dist < blink_threshold & curr_dist ~= 0);
+    if is_local_min & nonzero_below_threshold  
         blink_frame = [blink_frame,frame];
     end
 end
@@ -50,36 +52,25 @@ end
 shifted_blink_frame = [0 blink_frame(1:(length(blink_frame)-1))];
 blink_interval = blink_frame - shifted_blink_frame;
 
-% % blink_bin is a binary vector with 1 for frame with a blink, and 0 without
-% blink_bin = zeros(1,frame_cnt);
-% blink_bin(blink_frame) = 1;
+% get the full_blink_interval, which has the same length of total frames
+blink_cnt = 0;
+full_blink_interval = zeros(total_frame,1);
+for frame = 1 : total_frame
+    % get time interval over the entire time course
+    full_blink_interval(frame) = blink_interval(blink_cnt+1);
 
-% calculate the avg blink times over a fixed interval(avg_interval)
-% time interval for calculating average blink count.
-% avg_frame_window = frame_rate*avg_time_window;
-% window_cnt       = ceil(frame_cnt/avg_frame_window);
-% % padding
-% temp = zeros(1,window_cnt*avg_frame_window);
-% temp(1:length(blink_bin)) = blink_bin;
-% blink_bin = temp;
-% % MATLAB reshape is col-wise arrangement.
-% blink_bin = reshape(blink_bin, [avg_frame_window window_cnt]);
-% blink_bin = blink_bin';
-% % row-wise sum, get the blink count over each time window.
-% blink_cnt_avg = sum(blink_bin,2);
-% 
-% % convert blink_cnt_avg into a vector with the same dimension as
-% % blink_bin but blink_avg_y has the avg blink cnt within the window period.
-% blink_avg_y   = repmat(blink_cnt_avg,[1 avg_frame_window]);
-% blink_avg_y   = reshape(blink_avg_y.',1,[]);
-% blink_avg_y   = blink_avg_y(1:frame_cnt);
+    % if reach the next blink frame, increase blink_cnt
+    if blink_cnt ~= (length(blink_frame)-1) & frame == blink_frame(blink_cnt+1) 
+        blink_cnt = 1 + blink_cnt; 
+    end
+end
+full_blink_interval = full_blink_interval/frame_rate;
 
 % Output struct
 blink = [];
 % blink.avg_blink_cnt  = blink_avg_y;
 blink.avg_dist       = avg_dist;
-blink.blink_frame    = blink_frame;
-blink.blink_interval = blink_interval;
-blink.blink_interval = getBlinkInterval(fit,frame_rate);
+blink.blink_frame    = blink_frame';
+blink.blink_interval = full_blink_interval;
 end
 
