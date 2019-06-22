a57cf71add9fe45cc1ee905a66a0762a8a067c2e
Wanqiao Ding <wanqiaod@gmail.com>
2019-06-21 21:05:58 -0600

    Deleted dead code.

--- mfile/test_functions/test_run_pipeline.m
+++ mfile/test_functions/test_run_pipeline.m
@@ -9,12 +9,6 @@ run_zface    = false;
 run_FETA     = false;
 run_AU       = true;
 create_out   = false;
-% % Test initOutDir()
-% [zface,FETA,AU] = initOutDir(zface_folder,FETA_folder,AU_folder,output_dir,...
-% 							 false);
-% addpath(genpath('.'));
-
-% runZface(video_dir,zface);
 
 runPipeline(video_dir,output_dir,zface_folder,FETA_folder,AU_folder,...
             run_zface,run_FETA,run_AU,create_out);
