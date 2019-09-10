4d654bba757667fff36b890835b195ac61176728
Wanqiao Ding <wanqiaod@gmail.com>
2019-09-10 13:18:51 -0400

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   .gitignore
            modified:   example/AFAR_example.m
    
    Update the example code of running AFAR

--- example/AFAR_example.m
+++ example/AFAR_example.m
@@ -1,3 +1,17 @@
 
+video_dir = 'test_video/';
+out_dir   = 'test_out/';
+zface_dir = '../zface/';
+FETA_dir  = '../FETA/';
+AU_dir    = '../AU_detector/';
+
+run_zface = true;
+run_FETA  = true;
+run_AU    = true;
+
+runPipeline(video_dir,out_dir,zface_dir,FETA_dir,AU_dir,run_zface,run_FETA,...
+            run_AU,'zface_save_video',true);
+
+
 
 
