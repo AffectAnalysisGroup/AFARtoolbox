31b10c64c23e717d3688ad387eeb56035fc6e987
Wanqiao Ding <wanqiaod@gmail.com>
2019-09-23 12:07:36 -0400

    Add start/end frame option to runZfaceSingleVideo.m

--- mfile/runZfaceSingleVideo.m
+++ mfile/runZfaceSingleVideo.m
@@ -13,19 +13,25 @@ function runZfaceSingleVideo(zface_param,video_path,zface_video_path,...
 
     % Parse optional arguments
     p = inputParser;
-    default_verbose    = false;
-    default_log_fid    = -1;
-    default_save_fit   = true;
-    default_save_video = false;
+    default_verbose  = false;
+    default_log_fid  = -1;
+    default_save_fit = true;
+    default_save_video  = false;
+    default_start_frame = -1;
+    default_end_frame   = -1;
     addOptional(p,'verbose',default_verbose);
     addOptional(p,'log_fid',default_log_fid);
     addOptional(p,'save_fit',default_save_fit);
     addOptional(p,'save_video',default_save_video);
+    addOptional(p,'start_frame',default_start_frame);
+    addOptional(p,'end_frame',default_end_frame);
     parse(p,varargin{:}); 
-    verbose    = p.Results.verbose;   
-    log_fid    = p.Results.log_fid;
-    save_fit   = p.Results.save_fit;
-    save_video = p.Results.save_video;
+    verbose  = p.Results.verbose;   
+    log_fid  = p.Results.log_fid;
+    save_fit = p.Results.save_fit;
+    save_video  = p.Results.save_video;
+    start_frame = p.Results.start_frame;
+    end_frame   = p.Results.end_frame;
 
     if (~save_fit && ~save_video)
         % if not save fit or video, nothing to save, quit.
@@ -52,15 +58,32 @@ function runZfaceSingleVideo(zface_param,video_path,zface_video_path,...
     
     fit    = [];
     ctrl2D = [];
-    vo.CurrentTime = 0;
-    frame_index    = 0;
+    vo.CurrentTime  = 0;
+    frame_index     = 0;
+    fit_frame_index = 0;
     while hasFrame(vo)
         % Track each frame
         I = readFrame(vo);
-        if frame_index == 0 && save_video
+        if frame_index == 0 && save_video % first frame
             h = InitDisplay(zf,I);
         end
         frame_index = frame_index + 1;
+        
+        if (start_frame < 0 && end_frame < 0)
+        % If input arg doesn't specify the start/end frame, use frame_index.
+        % Otherwise, check if current frame_index is within the given range.
+            fit_frame_index = frame_index;
+        else
+            if (frame_index >= start_frame && frame_index <= end_frame)
+            % if input args specify the start/end frame, incr fit_frame_index 
+            % every iteration. Otherwise, skip this iteration.
+                fit_frame_index = fit_frame_index + 1;
+            else
+                continue;
+            end
+        end
+
+
       [ ctrl2D, mesh2D, mesh3D, pars ] = zf.Fit( I, ctrl2D );
         if save_video
             UpdateDisplay( h, zf, I, ctrl2D, mesh2D, pars );
@@ -69,22 +92,22 @@ function runZfaceSingleVideo(zface_param,video_path,zface_video_path,...
             writeVideo(vw,X);
         end
         
-        fit(frame_index).frame     = frame_index;
-        fit(frame_index).isTracked = ~isempty(ctrl2D);
-        if fit(frame_index).isTracked
-            fit(frame_index).pts_2d   = ctrl2D;
-            fit(frame_index).pts_3d   = mesh3D;
-            fit(frame_index).headPose = pars(4:6);
-            fit(frame_index).pdmPars  = pars;
+        fit(fit_frame_index).frame     = frame_index;
+        fit(fit_frame_index).isTracked = ~isempty(ctrl2D);
+        if fit(fit_frame_index).isTracked
+            fit(fit_frame_index).pts_2d   = ctrl2D;
+            fit(fit_frame_index).pts_3d   = mesh3D;
+            fit(fit_frame_index).headPose = pars(4:6);
+            fit(fit_frame_index).pdmPars  = pars;
         else
-            fit(frame_index).pts_2d   = [];
-            fit(frame_index).pts_3d   = [];
-            fit(frame_index).headPose = [];
-            fit(frame_index).pdmPars  = [];
+            fit(fit_frame_index).pts_2d   = [];
+            fit(fit_frame_index).pts_3d   = [];
+            fit(fit_frame_index).headPose = [];
+            fit(fit_frame_index).pdmPars  = [];
         end    
-        if mod(frame_index,500) == 0 && verbose 
+        if mod(fit_frame_index,500) == 0 && verbose 
             msg = sprintf('%s -- %d frames tracked from %s\n',getMyTime(),...
-                          frame_index,video_fname);
+                          fit_frame_index,video_fname);
             printWrite(msg,log_fid);
         end
 
