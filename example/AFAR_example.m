66f23f7417e8eb66be4aa098845b8f47c4b188b5
wanqiaod <wanqiaod@gmail.com>
2020-10-29 19:39:46 -0400

    Updated example code

--- example/AFAR_example.m
+++ example/AFAR_example.m
@@ -8,11 +8,14 @@ run_zface = true;
 run_FETA  = false;
 run_AU    = false;
 
-de_identify = true;
+verbose = true;
+save_zface_video = false;
+de_identify      = true;
 
-runPipeline(video_dir,out_dir,zface_dir,FETA_dir,AU_dir,run_zface,run_FETA,...
-            run_AU,'zface_save_video',true,'verbose',true,...
-            'de_identify',de_identify);
+runPipeline(video_dir,out_dir,zface_dir,FETA_dir,AU_dir,run_zface,run_FETA,run_AU,...
+            'zface_save_video',save_zface_video,...
+            'de_identify',de_identify...
+            'verbose',true);
 
 
 
